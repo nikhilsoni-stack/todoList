1873eac375e6a81e9c298e1bee5bcc643b77dbb3
Nikhil <nsoni@deqode.com>
2020-12-13 02:06:52 +0530

    Updates

---
+++ precache-manifest.d3fd796c112694986cb53214da22b188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8bbbb0fa38518aebf0b2e5333421beb",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "d9b0d4a7774b1dd6645e",
+    "url": "/todolist/static/css/main.0b888e22.chunk.css"
+  },
+  {
+    "revision": "60475932505251a595c3",
+    "url": "/todolist/static/js/2.26b680a5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist/static/js/2.26b680a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b0d4a7774b1dd6645e",
+    "url": "/todolist/static/js/main.f912de00.chunk.js"
+  },
+  {
+    "revision": "58ba8c451ba96e98ba1e",
+    "url": "/todolist/static/js/runtime-main.932aad63.js"
+  }
+]);
