a9f6298f441edfe0a24c183eed2da0029cb0ca64
Nikhil <nsoni@deqode.com>
2020-12-13 02:47:09 +0530

    fix api

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,27 +1,18 @@
 import axios from "axios";
 
 export const getTasks = async () => {
-  let config = {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
-      "Access-Control-Allow-Headers":
-        "Content-Type, Authorization, Content-Length, X-Requested-With",
-    },
-  };
   const data = await axios.get(
-    "https://git.heroku.com/mmy-to-do-list.git/api/tasks",
-    config
+    "https://mmy-to-do-list.herokuapp.com/api/tasks"
   );
   return data.data;
 };
 export const updateTask = async (task) => {
   const result = await axios.patch(
-    "https://git.heroku.com/mmy-to-do-list.git/api/tasks/" + task.id,
+    "https://mmy-to-do-list.herokuapp.com/api/tasks/" + task.id,
     task
   );
   const data = await axios.get(
-    "https://git.heroku.com/mmy-to-do-list.git/api/tasks"
+    "https://mmy-to-do-list.herokuapp.com/api/tasks"
   );
 
   return data.data;
@@ -29,21 +20,21 @@ export const updateTask = async (task) => {
 
 export const createTask = async (task) => {
   const result = await axios.post(
-    "https://mmy-to-do-list.herokuapp.com/api/tasks/api/tasks",
+    "https://mmy-to-do-list.herokuapp.com/api/tasks",
     task
   );
   const data = await axios.get(
-    "https://mmy-to-do-list.herokuapp.com/api/tasks/api/tasks"
+    "https://mmy-to-do-list.herokuapp.com/api/tasks/"
   );
   return data.data;
 };
 
 export const deletetask = async (taskId) => {
   const result = await axios.delete(
-    "https://mmy-to-do-list.herokuapp.com/api/tasks/api/tasks/" + taskId
+    "https://mmy-to-do-list.herokuapp.com/api/tasks/" + taskId
   );
   const data = await axios.get(
-    "https://mmy-to-do-list.herokuapp.com/api/tasks/api/tasks"
+    "https://mmy-to-do-list.herokuapp.com/api/tasks/"
   );
   return data.data;
 };
